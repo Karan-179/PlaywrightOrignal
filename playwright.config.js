476c24c8caa57c16b2dbd5e8b7bc7c01ff9be22b
Karan-179 <98122113+Karan-179@users.noreply.github.com>
2022-11-18 12:27:41 +0530

    Playwright setup
    Playwright first setup

---
+++ playwright.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+const config = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 50 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName : 'chromium'
+  },
+}
+module.exports = config
